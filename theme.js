65b8bf7aa7a050c235d104ede1a76c31bdcf2c11
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-06-30 19:46:02 +0200

    Mixin version of theme picker

--- theme.js
+++ theme.js
@@ -1,13 +1,7 @@
-
-const defaultTheme = (window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ? 'dark'
-    : 'light'
-document.body.setAttribute('data-theme', defaultTheme)
-
 const themeSelect = document.querySelector('select');
 
 themeSelect.addEventListener('change', (event) => {
     const newTheme = event.currentTarget.value;
-    if (!newTheme) { document.body.setAttribute('data-theme', defaultTheme) }
+    if (!newTheme) { document.body.removeAttribute('data-theme') }
     else { document.body.setAttribute('data-theme', newTheme) }
 })
